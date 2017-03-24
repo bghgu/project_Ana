3f10355075d16ccd22f4e240de85b78744f62624
DaSeul Bae <ektmf1993@gamil.com>
2017-03-24 10:56:24 +0900

    myPage edit

--- www/js/user.js
+++ www/js/user.js
@@ -13,7 +13,6 @@ angular.module('starter')
         })
         .success(function(data) {
           $ionicLoading.hide();
-          console.log(data);
           $localstorage.setObject('user', data);
           $scope.myPage = $localstorage.getObject('user').dto;
         })
@@ -34,21 +33,56 @@ angular.module('starter')
       $location.path('/login');
     }
 
+
+    //회원정보 수정
     /////////////////////////////////////////
     $scope.update = function(data) {
-      //$ionicLoading.show();
-      console.log(data);
+      $ionicLoading.show();
+      //기본값 설정
       //////////////////////////////////
-      if (typeof(data) == 'undefined') {
-        data = $localstorage.getObject('user').dto;
+      if(typeof(data.address) == 'undefined') {
+        data.address = $localstorage.getObject('user').dto.address;
+      }
+      if(typeof(data.email) == 'undefined') {
+        data.email = $localstorage.getObject('user').dto.email;
+      }
+      if(typeof(data.jobName) == 'undefined') {
+        data.jobName = $localstorage.getObject('user').dto.jobName;
+      }
+      if(typeof(data.jobPhone) == 'undefined') {
+        data.jobPhone = $localstorage.getObject('user').dto.jobPhone;
+      }
+      if(typeof(data.jobStatus) == 'undefined') {
+        data.jobStatus = $localstorage.getObject('user').dto.jobStatus;
+      }
+      if(typeof(data.openAddress) == 'undefined') {
+        data.openAddress = $localstorage.getObject('user').dto.openAddress;
+      }
+      if(typeof(data.openBirth) == 'undefined') {
+        data.openBirth = $localstorage.getObject('user').dto.openBirth;
+      }
+      if(typeof(data.openEmail) == 'undefined') {
+        data.openEmail = $localstorage.getObject('user').dto.openEmail;
+      }
+      if(typeof(data.openJobName) == 'undefined') {
+        data.openJobName = $localstorage.getObject('user').dto.openJobName;
+      }
+      if(typeof(data.openJobPhone) == 'undefined') {
+        data.openJobPhone = $localstorage.getObject('user').dto.openJobPhone;
+      }
+      if(typeof(data.openJobStatus) == 'undefined') {
+        data.openJobStatus = $localstorage.getObject('user').dto.openJobStatus;
+      }
+      if(typeof(data.openPhone) == 'undefined') {
+        data.openPhone = $localstorage.getObject('user').dto.openPhone;
+      }
+      if(typeof(data.openPhoto) == 'undefined') {
+        data.openPhoto = $localstorage.getObject('user').dto.openPhoto;
       }
       //////////////////////////////////
-
       if ($localstorage.getObject('token')) {
-        console.log(data);
         $http({
             method: 'post',
-            //url: 'http://:9303/user/update',
             url: 'http://bghgu.iptime.org:9303/user/update',
             headers: {
               'Content-Type': 'application/json',
@@ -57,39 +91,44 @@ angular.module('starter')
             data: ({
               loginId: $localstorage.getObject('user').dto.loginId,
               password: $localstorage.getObject('user').dto.password,
+              cnumber:  $localstorage.getObject('user').dto.cnumber,
+
+              photo: $localstorage.getObject('user').dto.photo,
+              openPhoto: data.openPhoto,
+
+              address: data.address,
               email: data.email,
-              address: data.adress,
               jobName: data.jobName,
               jobPhone: data.jobPhone,
               jobStatus: data.jobStatus,
-              photo: data.photo,
+
+              openAddress: data.openAddress,
               openBirth: data.openBirth,
-              openPhone: data.openPhone,
               openEmail: data.openEmail,
-              openAddress: data.openEmail,
               openJobName: data.openJobName,
               openJobPhone: data.openJobPhone,
-              openJobStatus: data.openjobStatus,
-              openPhoto: data.openPhoto,
-              cnumber:  $localstorage.getObject('user').dto.cnumber
+              openJobStatus: data.openJobStatus,
+              openPhone: data.openPhone,
             })
           })
           .success(function(data) {
             $ionicLoading.hide();
-            console.log(data);
+            $ionicPopup.alert({
+              title: 'success',
+              template: '수정 완료'
+            });
             $localstorage.setObject('user', data);
             $scope.myPage = $localstorage.getObject('user').dto;
           })
           .error(function(data, status, headers, config) {
-            console.log(data);
             $ionicLoading.hide();
             $ionicPopup.alert({
               title: 'Warning Message',
               template: '잠시후 다시 시도해 주세요.'
             });
-            //$location.path('/myPage');
+            $location.path('/myPage');
           });
       }
+      /////////////////////////////////////////
     };
-    /////////////////////////////////////////
   });
